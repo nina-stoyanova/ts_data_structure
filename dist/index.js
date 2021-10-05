c17a742a7936f7a7938b21c9fdd1378e75a82796
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-10-05 09:11:41 +0300

    typescript

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testFn = void 0;
+function test(number) {
+    console.log("jahduasbhldsab");
+    return number * 5;
+}
+function logName(obj) {
+    console.log(obj.name);
+}
+function logNameFromInterface(obj) {
+    console.log(obj.name);
+}
+test(5);
+exports.testFn = test;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+function test(number: number) {
+    console.log("jahduasbhldsab")
+    return number * 5;
+}
+
+function logName(obj: { name: string }) {
+    console.log(obj.name)
+}
+
+interface objectWithName {
+    name: string
+}
+
+function logNameFromInterface(obj: objectWithName) {
+    console.log(obj.name)
+}
+
+test(5)
+
+export const testFn = test
